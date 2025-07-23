daaac161760098f4dd2f4312697ee4c0c53a5dec
iwaki-toshiyuki <toytoy0714@icloud.com>
2025-07-23 15:08:31 +0900

    fix:Ruffの設定

--- src/closet_search/asgi.py
+++ src/closet_search/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'closet_search.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "closet_search.settings")
 
 application = get_asgi_application()
