d7056d4fa75858e9f10512962df60c733dd2711f
ootamaru <waza.chika@gmail.com>
2025-07-31 23:53:58 +0900

    fix:hello worldの表示に伴うファイルの変更

--- src/items/views.py
+++ src/items/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
+
+
 # from django.shortcuts import render
-# (Ruffにて引っかかるため、↑コメントアウト)
+# (Ruffエラーのため上記コメントアウト)
 # Create your views here.
+def test(request):
+    return HttpResponse("<h1> hello world</h1>")
