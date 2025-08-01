31515d18080a2ac079833758ef2a2485cc00c271
ootamaru <waza.chika@gmail.com>
2025-08-01 21:00:35 +0900

    Revert "fix:hello worldの表示に伴うファイルの変更"
    This reverts commit d7056d4fa75858e9f10512962df60c733dd2711f.

--- src/closet_search/urls.py
+++ src/closet_search/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("items.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
--- src/items/views.py
+++ src/items/views.py
@@ -1,8 +1,3 @@
-from django.http import HttpResponse
-
-
 # from django.shortcuts import render
-# (Ruffエラーのため上記コメントアウト)
+# (Ruffにて引っかかるため、↑コメントアウト)
 # Create your views here.
-def test(request):
-    return HttpResponse("<h1> hello world</h1>")
