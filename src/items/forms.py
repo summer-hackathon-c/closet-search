1e41ba8aa5eb851567325936e25970d25f02644f
ootamaru <waza.chika@gmail.com>
2025-08-09 21:59:28 +0900

    fix:make format実行

--- src/items/forms.py
+++ src/items/forms.py
@@ -1,32 +1,31 @@
 from django import forms
 from .models import User
 
-#新規登録
-class CustomUserCreationForm(forms.ModelForm):
-    #入力フィールドへ入力したパスワードは非表示で表示
-    password1 = forms.CharField(
-        label='',
-        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
-        )
-    password2 = forms.CharField(
-        label = '',
-        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
-        )
 
-    #以下のクラスでモデルとフィールドを追加
+# 新規登録
+class CustomUserCreationForm(forms.ModelForm):
+    # 入力フィールドへ入力したパスワードは非表示で表示
+    password1 = forms.CharField(
+        label="", widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password2 = forms.CharField(
+        label="", widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    # 以下のクラスでモデルとフィールドを追加
     class Meta:
         model = User
         fields = ("user_name", "email")
-    
-    #ユーザーネームとメールアドレスとプレースホルダーとして表示(ラベル表示を非表示)
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields["user_name"].label=""
-        self.fields["user_name"].widget.attrs["placeholder"] ="user name"
-        self.fields["email"].label=""
+
+    # ユーザーネームとメールアドレスとプレースホルダーとして表示(ラベル表示を非表示)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["user_name"].label = ""
+        self.fields["user_name"].widget.attrs["placeholder"] = "user name"
+        self.fields["email"].label = ""
         self.fields["email"].widget.attrs["placeholder"] = "email"
 
-    #2つのパスワードが一致しているかの確認
+    # 2つのパスワードが一致しているかの確認
     def clean(self):
         cleaned_data = super().clean()
         password1 = cleaned_data.get("password1")
@@ -34,9 +33,9 @@ class CustomUserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("パスワードが一致しません")
         return cleaned_data
-    
-    #パスワードをハッシュ化して保存する
-    def save(self,commit=True):
+
+    # パスワードをハッシュ化して保存する
+    def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
 
@@ -44,11 +43,9 @@ class CustomUserCreationForm(forms.ModelForm):
             user.save()
         return user
 
-    #emailの重複確認
+    # emailの重複確認
     def clean_email(self):
         email = self.cleaned_data.get("email")
-        if User.objects.filter(email=email).exists(): 
+        if User.objects.filter(email=email).exists():
             raise forms.ValidationError("このメールアドレスは既に登録されています。")
         return email
-    
-    
