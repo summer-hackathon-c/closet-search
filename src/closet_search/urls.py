cc0899470b1303a0986a45bdd348de959d7acbab
ootamaru <waza.chika@gmail.com>
2025-08-09 21:59:28 +0900

    fix:make format実行

--- src/closet_search/urls.py
+++ src/closet_search/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("items.urls"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("items.urls"))]
--- src/items/urls.py
+++ src/items/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from . import views
 from .views import SignUpView
 
 
-urlpatterns = [
-    path("signup/",SignUpView.as_view(), name="signup")
-]
+urlpatterns = [path("signup/", SignUpView.as_view(), name="signup")]
