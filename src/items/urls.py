1fb63232ad9c312cd604c24d10332e9ef26ab046
ootamaru <waza.chika@gmail.com>
2025-08-13 21:26:24 +0900

    fix:ログイン機能の実装(バックエンド)

--- src/items/urls.py
+++ src/items/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView,UserLoginView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/",UserLoginView.as_view(), name="login"),
 ]
