a3d34cc633e5194ae5ec0c0b30c4f09346756c42
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-08-14 21:32:22 +0900

    fix:make format実行

--- src/items/urls.py
+++ src/items/urls.py
@@ -5,7 +5,7 @@ from .views import SignUpView, UserLoginView, ItemCreateView, ItemListView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path('', RedirectView.as_view(url='/login/', permanent=False)),
+    path("", RedirectView.as_view(url="/login/", permanent=False)),
     path("login/", UserLoginView.as_view(), name="login"),
     path("items/create/", ItemCreateView.as_view(), name="item-create"),
     path("items/", ItemListView.as_view(), name="item-list"),
