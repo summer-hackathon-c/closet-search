abad4ab0219d55e7a165b3a7b9a27aba674c91d7
ootamaru <waza.chika@gmail.com>
2025-08-14 23:07:53 +0900

    fix:アイテム一覧表示の機能追加

--- src/items/urls.py
+++ src/items/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("items/create/", ItemCreateView.as_view(), name="item-create"),
-    path("items/", ItemListView.as_view(), name="item-list"),
+    path("items/<int:user_id>/", ItemListView.as_view(), name="item-list"),
 ]
--- src/items/views.py
+++ src/items/views.py
@@ -2,6 +2,7 @@
 import os
 import uuid
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin  # 上位に記載必要
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login, get_user_model
 from django.views import View
@@ -10,16 +11,14 @@ from django.urls import reverse_lazy
 from django.utils.timezone import now
 from django.shortcuts import render, redirect
 from django.core.files.storage import default_storage
+from django.http import Http404
 from .models import Item, ItemPhoto
 from .forms import CustomUserCreationForm, LoginForm, ItemCreateForm, PhotoUploadForm
 
 User = get_user_model()
 
-# from .models import User
 
 # Create your views here.
-
-
 # ユーザー新規登録
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -44,14 +43,37 @@ class UserLoginView(LoginView):
         return super().form_valid(form)  # 標準のリダイレクト処理などを実行
 
     def get_success_url(self):
-        return reverse_lazy("index")
+        return reverse_lazy("item-list", kwargs={"user_id": self.request.user.id})
 
 
-# アイテム一覧
-class ItemListView(ListView):
-    model = Item
+# アイテム一覧表示
+class ItemListView(LoginRequiredMixin, ListView):
     template_name = "items/index.html"
-    context_object_name = "items"
+    context_object_name = "items"  # テンプレートにて使用する変数名
+
+    # 一覧表示に必要な情報（モデルから取り出したデータの集まり）を定義
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")  # URLパラメーターよりuser_idを取得
+        try:
+            user = User.objects.get(pk=user_id)  # user_idを使用しユーザー情報を取得
+        except User.DoesNotExist:
+            raise Http404("ユーザーが見つかりません")
+
+        # ログイン状態であっても、他人のデータへアクセスできないように設定
+        if self.request.user != user:
+            raise Http404("ほかのユーザーのアイテムは表示できません")
+
+        self.profile_user = user  # contextに渡すために一時的に保存
+
+        return Item.objects.filter(
+            user=user, delete_flag=False
+        )  # ユーザーに紐づく、論理削除されていないアイテムを取得
+
+    # profile_userをテンプレートにて使用するため定義
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile_user"] = self.profile_user
+        return context
 
 
 # アイテム新規登録
