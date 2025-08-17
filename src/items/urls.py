d4e25152945c56af1e74e3b08d743d63ced6f534
ootamaru <waza.chika@gmail.com>
2025-08-18 06:04:28 +0900

    fix:deleteimeブランチ：指示にしたがいurls.pyのL21-22を削除

--- src/items/urls.py
+++ src/items/urls.py
@@ -18,7 +18,5 @@ urlpatterns = [
     path("items/create/", ItemCreateView.as_view(), name="item-create"),
     path("items/", ItemListView.as_view(), name="item-list"),
     path("items/<int:pk>", ItemDetailView.as_view(), name="item-detail"),
-    # TODO : 削除メソッド追加時、L22は削除してください。エラーにならないよう一旦Logoutに紐づけているだけですmm
-    # path("<int:pk>/delete/", LogoutView.as_view(), name="item-delete"),
     path("items/<int:pk>/delete/", ItemDeleteView.as_view(), name="item-delete"),
 ]
